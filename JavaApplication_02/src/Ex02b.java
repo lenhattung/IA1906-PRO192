f78c8d5743aab4339db057725b6e6de850313f80
Tung Le <lenhattung@gmail.com>
2025-01-10 13:18:15 +0700

    Update Ex02b.java

--- JavaApplication_02/src/Ex02b.java
+++ JavaApplication_02/src/Ex02b.java
@@ -15,5 +15,16 @@ public class Ex02b {
         System.out.println(a/b);// 2
         System.out.println(a%b);// 1
         System.out.println((double)a/b);
+        
+        int x = 5;
+        System.out.println(x++);//5
+        System.out.println(++x);//7
+        System.out.println(--x);//6
+        System.out.println(x--);//6
+        // x=5
+        
+        int y = 5;
+        int z = 4;
+        System.out.println(y++ - ++z);
     }
 }
