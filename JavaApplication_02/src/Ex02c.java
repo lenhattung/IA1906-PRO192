31a790176b8efec3a2b05701c5a3f860b8394562
Tung Le <lenhattung@gmail.com>
2025-01-10 13:41:43 +0700

    Update Ex02c.java

--- JavaApplication_02/src/Ex02c.java
+++ JavaApplication_02/src/Ex02c.java
@@ -9,33 +9,31 @@
  * @author tungi
  */
 public class Ex02c {
-    public static void main(String[] args){
-        int a = 5;
-        int b = 2;
-        System.out.println(a/b);// 2
-        System.out.println(a%b);// 1
-        System.out.println((double)a/b);
-        
-        int x = 5;
-        System.out.println(x++);//5
-        System.out.println(++x);//7
-        System.out.println(--x);//6
-        System.out.println(x--);//6
-        // x=5
-        
-        int y = 5;
-        int z = 4;
-        System.out.println(y++ - ++z);
-        
+
+    public static void main(String[] args) {
+        boolean check = 5 >= 4;
+        System.out.println(check);
+
+        check = ((5 >= 4) && (15 % 2 == 0));
+        System.out.println(check);
         
         // 
-        int t = 15;
-        System.out.println(t>>1);
-        t=t>>1;
-        System.out.println(t);
-        t=t<<1;
-        System.out.println(t);
-        t=t<<1;
-        System.out.println(t);
+        int x = 100;
+        // (condition)?op1:op2;
+        System.out.println((x%2==0)?"chan":"le");
+        
+        /*
+        String ketQua = "";
+        if(x%2==0){
+            ketQua = "chan";
+        }else{
+            ketQua = "le";
+        }
+        
+        ketQua = (x%2==0)?"chan":"le";
+        */
+        
+        int z = 5;
+        z+=10; // z = z+10;
     }
 }
