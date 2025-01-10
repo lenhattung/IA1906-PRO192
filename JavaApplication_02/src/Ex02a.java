393b2c6df8c17636898180db703ebe51c245f1d3
Tung Le <lenhattung@gmail.com>
2025-01-10 13:06:06 +0700

    update
    updare

---
+++ JavaApplication_02/src/Ex02a.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class Ex02a {
+    public static void main(String[] args){
+        char c = 'c';
+        byte b = 100;
+        short s = 2000;
+        int i = 1000000;
+        long l = 1000000L;
+        float f = 1.5F;
+        double d = 1.5;
+        boolean $check = true;
+        
+        // Double d1 = new Double(1000000);
+    }
+}
---
+++ JavaApplication_02/src/Ex02b.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class Ex02b {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 2;
+        System.out.println(a/b);// 2
+        System.out.println(a%b);// 1
+        System.out.println((double)a/b);
+    }
+}
