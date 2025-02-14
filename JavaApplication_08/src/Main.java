075ad0b7cc51e19dda51bc229854dd1e35e342a0
Tung Le <lenhattung@gmail.com>
2025-02-14 14:11:26 +0700

    ud
    ud

---
+++ JavaApplication_08/src/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        String c = "";
+        
+        // c = a+b;
+        
+        // syntax error
+        // compile error
+    }
+}
---
+++ JavaApplication_08/src/Main2.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author tungi
+ */
+public class Main2 {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a: ");
+        a = sc.nextInt();
+        System.out.println("Input b: ");
+        b = sc.nextInt();
+
+        System.out.println(a + b);
+
+        /*
+Input a: 
+5
+Input b: 
+ten
+Exception in thread "main" java.util.InputMismatchException
+	at java.util.Scanner.throwFor(Scanner.java:864)
+	at java.util.Scanner.next(Scanner.java:1485)
+	at java.util.Scanner.nextInt(Scanner.java:2117)
+	at java.util.Scanner.nextInt(Scanner.java:2076)
+	at Main2.main(Main2.java:23)
+C:\Users\tungi\AppData\Local\NetBeans\Cache\8.2\executor-snippets\run.xml:53: Java returned: 1
+BUILD FAILED (total time: 10 seconds)
+
+        
+        => Runtime Error
+         */
+    }
+}
